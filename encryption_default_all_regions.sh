3655737a98e853d6163e9bfc6a7ee1686985c63f
Troy Knutson <gnuty80@gmail.com>
2021-04-02 17:17:41 -0700

    encrypt EBS by default in all regions

---
+++ encryption_default_all_regions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+## Copyright - Troy Knutson
+## Enables EBS encryptionn by default in all regions
+
+#set -o xtrace
+#set -o pipefail
+#set -o errexit
+
+## Loop through all regions
+for region in $(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+do
+  aws ec2 enable-ebs-encryption-by-default --region ${region} | grep "true" > /dev/null 2>&1
+  [ $? -eq "0" ] && echo "$(echo $(date +%Y-%m-%d) $(date +%H:%M:%S%Z)) INFO EBS encryption enabled by default in ${region}"
+done
+
