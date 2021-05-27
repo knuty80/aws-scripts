6b5d61fec66ae47ff284b15d5a25ca144275c098
Troy Knutson <gnuty80@gmail.com>
2021-05-27 13:23:44 -0700

    ensuring the region is set

--- console_output.sh
+++ console_output.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
 ## Gets console output from an ec2 instance
 ## Pass the instance ID as the 1st argument or enter it when prompted
-## Uses your default region unless you override it; Default is set to us-west-2
+## Uses your default region unless you override it; Default is set to 
 
 set -o errexit
 set -o pipefail
 #set -o xtrace
 
-## Setting the default if no region is found or passed as arg2 input
-no_region_default='us-west-2'
+### Setting the default if no region is found or passed as arg2 input; Will ask for input!
+no_default_region='us-east-1'
 
-#### Ensure two and only two args are passed
-## Instance ID
-instanceId="${1}"
-## Default AWS region
-[ ! -f "~/.aws/config" ] && region="$(grep 'region' ~/.aws/config | awk -F '=' '{print $2}' | head -1)" || region=${2:-${no_region_default}}
+## Instance ID passed as arg1 or ask for it
+instance_id=${1}
+[ -z ${instance_id} ] && read -p 'Instance ID: ' instance_id
 
-## Ask for the instance ID and the region, if they are not passed as args; arg1=instance_id arg2=region
-[ ! -z "$1" ] || read -p 'Instance ID: ' instanceId
-[ -n ${region} ] && read -p "Region [$(echo ${no_region_default})]: " region
-## region defaults to 'no_region_default' variable defined above, if region input from user is blank
-[ -n ${region} ] && region=${no_region_default}
+## Region passed as arg2 or look for it in AWS CLI config and ask the user to use or override it
+region=${2}
+if [ -z ${region} ]; then
+    if [ ! -f '~/.aws/config' ]; then
+		    region_default="$(grep 'region' ~/.aws/config | awk -F '=' '{print $2}' | head -1)"
+		else
+		    region_default="${no_default_region}"
+	  fi
+    ## Prompt user with default region or let them override it
+    read -p "Region [$(echo ${region_default})]: " region
+    [ -z ${region} ] && region=${region_default}
+fi
 
-aws ec2 get-console-output --region ${region} --instance-id ${instanceId} --output text
+## Execute the AWS CLI 'get-console-output' command
+## Ref: https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/get-console-output.html
+aws ec2 get-console-output --region ${region} --instance-id ${instance_id} --output text
 
