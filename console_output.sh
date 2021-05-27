9622a4d2bc7828d739e60e2d23ddffc0c5e5840e
Troy Knutson <gnuty80@gmail.com>
2021-05-27 11:16:07 -0700

    improved default region selection if not passed as arg2 by user

--- console_output.sh
+++ console_output.sh
@@ -7,17 +7,20 @@ set -o errexit
 set -o pipefail
 #set -o xtrace
 
+## Setting the default if no region is found or passed as arg2 input
+no_region_default='us-west-2'
+
 #### Ensure two and only two args are passed
 ## Instance ID
 instanceId="${1}"
 ## Default AWS region
-#[ ! -f "~/.aws/config" ] && region="$(grep 'region' ~/.aws/config | awk -F '=' '{print $2}' | head -1)" || region=${2}
+[ ! -f "~/.aws/config" ] && region="$(grep 'region' ~/.aws/config | awk -F '=' '{print $2}' | head -1)" || region=${2:-${no_region_default}}
 
 ## Ask for the instance ID and the region, if they are not passed as args; arg1=instance_id arg2=region
-## region defaults to us-west-2...
 [ ! -z "$1" ] || read -p 'Instance ID: ' instanceId
-[ ! -n ${region} ] && read -p 'Region [us-west-2]: ' region
-[[ ${region} == "" ]] && region=${2:-us-west-2}
+[ -n ${region} ] && read -p "Region [$(echo ${no_region_default})]: " region
+## region defaults to 'no_region_default' variable defined above, if region input from user is blank
+[ -n ${region} ] && region=${no_region_default}
 
 aws ec2 get-console-output --region ${region} --instance-id ${instanceId} --output text
 
